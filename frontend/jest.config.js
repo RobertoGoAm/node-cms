481058689beea8aa03c5216e109ef285be3b49e4
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-02 01:27:40 +0200

    Añadida la parte del fronted y cambiada la configuracion del proyecto para usar Jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ["json", "lcov", "text", "clover"],
+
+  collectCoverageFrom: [
+    "<rootDir>/src/app/**/*.ts",
+    "!<rootDir>/src/app/**/index.ts",
+    "!<rootDir>/src/app/**/*.module.ts",
+  ],
+
+  testPathIgnorePatterns: [
+    "<rootDir>/coverage/",
+    "<rootDir>/dist/",
+    "<rootDir>/e2e/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/src/app/*.(js|scss)",
+  ],
+
+  testMatch: ["<rootDir>/src/app/*.spec.ts", "<rootDir>/src/app/**/*.spec.ts"],
+};
