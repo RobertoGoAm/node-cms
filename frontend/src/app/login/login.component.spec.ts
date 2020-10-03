02271404cc3f30203652ac59c4c6fac793403e50
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-04 01:35:05 +0200

    Añadidos tests y funciones de validacion para el formulario de login

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let email: AbstractControl;
+  let password: AbstractControl;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +20,10 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // Form Elements
+    email = component.loginForm.controls.email;
+    password = component.loginForm.controls.password;
   });
 
   it('should create', () => {
@@ -31,5 +37,33 @@ describe('LoginComponent', () => {
     expect(element.querySelector('#email')).toBeTruthy();
     expect(element.querySelector('#password')).toBeTruthy();
     expect(element.querySelector('button[type=submit]')).toBeTruthy();
+  });
+
+  it('should validate email field as required', () => {
+    expect(email.valid).toBeFalsy();
+    expect(email.errors.required).toBeTruthy();
+  });
+
+  it('should validate password field as required', () => {
+    expect(password.valid).toBeFalsy();
+    expect(password.errors.required).toBeTruthy();
+  });
+
+  it('should fail to validate with bad email', () => {
+    email.setValue('badEmail');
+
+    const errors = email.errors;
+
+    expect(errors.pattern).toBeTruthy();
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('should succeed to validate with a good email', () => {
+    email.setValue('test@gmail.com');
+
+    const errors = email.errors || {};
+
+    expect(errors.pattern).toBeFalsy();
+    expect(email.valid).toBeTruthy();
   });
 });
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +8,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  // Pattern to check email (source: 'https://emailregex.com/')
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({});
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+      password: ['', Validators.required],
+    });
   }
 }
