2f98b66ff1570d8e8c142a850a6acac10d5cb146
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-07 01:25:36 +0200

    Funcionalidad inicial para la funcion de login en el frontend

--- frontend/src/app/auth/shared/auth.service.ts
+++ frontend/src/app/auth/shared/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +10,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string): void {}
+  login(email: string, password: string) {
+    return this.http.post(API_URL + '/login', { email, password });
+  }
 }
