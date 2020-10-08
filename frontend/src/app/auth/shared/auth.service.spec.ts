8f4d3e1fd464d83afe7ccb93b14e413a40c93a38
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-09 01:36:58 +0200

    Test para el servicio de auth del frontend

--- frontend/src/app/auth/shared/auth.service.spec.ts
+++ frontend/src/app/auth/shared/auth.service.spec.ts
@@ -1,17 +1,35 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 
 import { AuthService } from './auth.service';
 
+const API_URL = environment.apiUrl;
+
 describe('AuthService', () => {
-  beforeEach(() =>
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-    }).compileComponents()
-  );
+      imports: [HttpClientTestingModule]
+    }).compileComponents();
+  });
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
+
     expect(service).toBeTruthy();
   });
+
+  it('should login', () => {
+      const service: AuthService = TestBed.get(AuthService);
+      const backend: HttpTestingController = TestBed.get(HttpTestingController);
+
+      service.login('test@email.com', 'p4$w004rd').subscribe(result => {
+        expect(result).toEqual('Success!');
+      });
+
+      backend.expectOne({
+        method: 'POST',
+        url: API_URL + '/login'
+      }).flush('Success!');
+    });
 });
