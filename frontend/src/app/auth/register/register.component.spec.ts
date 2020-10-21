aea9abf10ed9546e8e61650e72053d08945ae4be
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-22 01:31:01 +0200

    Primeros pasos para el registro

---
+++ frontend/src/app/auth/register/register.component.spec.ts
@@ -0,0 +1,46 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
+import { MockAuthService } from '../shared/mock-auth';
+import { RegisterComponent } from './register.component';
+
+describe('registerComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+  let elements: HTMLElement;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      providers: [
+        {
+          provide: AuthService,
+          useClass: MockAuthService,
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    elements = fixture.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render login form', () => {
+    expect(elements.querySelector('form')).toBeTruthy();
+    expect(elements.querySelector('#name')).toBeTruthy();
+    expect(elements.querySelector('#email')).toBeTruthy();
+    expect(elements.querySelector('#password')).toBeTruthy();
+    expect(elements.querySelector('#passwordConfirm')).toBeTruthy();
+    expect(elements.querySelector('button[type=submit]')).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/auth/register/register.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  styleUrls: ['register.component.scss'],
+  template: `
+    <div>
+
+    </div>
+  `
+})
+export class RegisterComponent {
+  constructor() {}
+}
